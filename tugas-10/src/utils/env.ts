d9b8527fb1c1a8f0fb5cd3d7fac6b28bbb48a8f2
daffaf <daffafadila00@gmail.com>
2024-06-22 16:48:04 +0700

    tugas-10

---
+++ tugas-10/src/utils/env.ts
@@ -0,0 +1,7 @@
+export const CLOUDINARY_API_KEY: string = process.env.CLOUDINARY_API_KEY || "";
+export const CLOUDINARY_API_SECRET: string =
+  process.env.CLOUDINARY_API_SECRET || "";
+export const CLOUDINARY_CLOUD_NAME: string =
+  process.env.CLOUDINARY_CLOUD_NAME || "";
+export const DATABASE_URL: string = 
+"mongodb+srv://daffafadila00:daffafadila00@belajar-nodejs.1lyu0nb.mongodb.net/?retryWrites=true&w=majority&appName=belajar-nodejs";
